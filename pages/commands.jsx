aaf79a4f2c4e845b63235f3c94f1f33b1f9779b8
windssq <166147548+windssq@users.noreply.github.com>
2024-06-02 19:52:44 +0300

    Update commands.jsx

--- pages/commands.jsx
+++ pages/commands.jsx
@@ -3,32 +3,32 @@ import Link from "next/link";
 
 export default function Commands() {
 const commands = [
-  "/autoplay",
-  "/back",
-  "/statistic",
-  "/channel",
-  "/clear",
-  "/dj",
-  "/filter",
-  "/help",
-  "/language",
-  "/loop",
-  "/nowplaying",
-  "/pause",
+  "/yardım",
+  "/ban",
+  "/forceban",
+  "/oto-rol",
+  "/timeout-sistemi",
+  "/giriş-çıkış | /giriş-çıkış-kapat",
+  "/capslock-koruma",
+  "/mod-log",
+  "/kilitle",
+  "/yasaklı-kelime | /yasaklı-kelime-kapat",
+  "/sunucu-kur",
+  "/yavaş-mod",
+  "/sil",
+  "/buton-rol",
+  "/say",
+  "/sunucu-bilgi",
+  "/kullanıcı-bilgi",
+  "/hesap-koruma",
+  "/level-sistemi",
+  "/destek-sistemi",
+  "/özel-oda-sistemi",
+  "/özel-oda-kullanıcı-menü",
+  "/kayıt-sistemi",
+  "/afk",
   "/ping",
-  "/play",
-  "/playlist",
-  "/queue",
-  "/resume",
-  "/save",
-  "/search",
-  "/seek",
-  "/servers",
-  "/shuffle",
-  "/skip",
-  "/stop",
-  "/time",
-  "/volume"
+  "/kurucu"
 ];
 
   return (
@@ -42,7 +42,7 @@ const commands = [
          Bot Commands
         </p>
         <p className="text-white text-sm text-opacity-50 mb-5">
-        You can get information about the commands of the MusicMaker Bot.
+        You can get information about the commands of the CostaV Bot.
         </p>
 
         <div>
@@ -89,23 +89,23 @@ const commands = [
 
         <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
   
-  <Link href={"https://discord.com/api/oauth2/authorize?client_id=774043716797071371&permissions=277028620608&scope=applications.commands%20bot"}>
+  <Link href={"https://discord.com/oauth2/authorize?client_id=1244620556641239060&scope=bot&permissions=27648860222"}>
   <a className={"flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Invite MusicMaker</a>
+Invite CostaV</a>
   </Link>
 
   <div className="py-10"></div>
   
-  <Link href={"https://discord.gg/ZcDprCrFTn"}>
+  <Link href={"https://discord.gg/costav"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
 Support Server</a>
   </Link>
 
   <div className="py-10"></div>
   
-  <Link href={"https://top.gg/bot/774043716797071371/vote"}>
+  <Link href={"https://top.gg/bot/1244620556641239060"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Vote MusicMaker</a>
+Vote CostaV</a>
   </Link>
 </div>
 
