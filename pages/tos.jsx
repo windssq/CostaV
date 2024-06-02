e30530f6929a84f93e02bce59da1930c50479d69
windssq <166147548+windssq@users.noreply.github.com>
2024-06-02 20:05:15 +0300

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -2,9 +2,9 @@ export default function Tos() {
   return (
     <div>
      <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
-      <p className="text-sm text-white/30 mb-1">Last Update: 20/05/2022 🆕️</p>
+      <p className="text-sm text-white/30 mb-1">Last Update: 02/06/2024 🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-<p>⊳ Every discord server can use the MusicMaker bot for free, it is completely prohibited to sell or market for a fee.</p>
+<p>⊳ Every discord server can use the CostaV bot for free, it is completely prohibited to sell or market for a fee.</p>
 <div className="py-3"></div>
 <p>⊳ You cannot use bot commands to harm the bot.</p>
 <div className="py-3"></div>
@@ -14,7 +14,7 @@ export default function Tos() {
 <div className="py-3"></div>
 <p>⊳ While using the bot, you cannot use it in a way that harms other users for troll purposes.</p>
 <div className="py-3"></div>
-<p>⊳ To use certain discord bot commands, you need to vote for the <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">MusicMaker</a> bot at certain intervals.</p>
+<p>⊳ To use certain discord bot commands, you need to vote for the <a className="text-white" href="https://top.gg/bot/1244620556641239060">CostaV</a> bot at certain intervals.</p>
 </div>
     </div>
   );
